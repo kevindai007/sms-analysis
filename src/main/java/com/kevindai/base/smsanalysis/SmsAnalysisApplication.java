01f87965170c97028bfea259704c576b0a4d1a9f
daiwenkai <741007187@qq.com>
2025-09-21 09:57:28 +0400

    first commit

---
+++ src/main/java/com/kevindai/base/smsanalysis/SmsAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.kevindai.base.smsanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kevindai/base/smsanalysis/SmsAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevindai.base.smsanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
